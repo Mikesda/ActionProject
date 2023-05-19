6f00d4b86aef279c0a3f1ce5d1065ac5d85e0186
mike.sda@hotmail.com <mike.sda@hotmail.com>
2023-05-19 17:09:36 -0300

    Atualizado

--- js/crud.js
+++ js/crud.js
@@ -1,9 +1,29 @@
 document.querySelector("#salvar").addEventListener("click", cadastrar);
 
-let tarefas = []
+let lista_tarefas = []
 
 window.addEventListener("load", () => {
-    tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas")) || []
+    atualizar()
+})
+
+document.querySelector("#pendentes").addEventListener("click",() => {
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas")) || []
+    lista_tarefas = lista_tarefas.filter(tarefa => !tarefa.concluida)
+    atualizar()
+})
+
+document.querySelector("#busca").addEventListener("keyup", () => {
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas")) || []
+    const nome = document.querySelector("busca").value
+    lista_tarefas=
+        lista_tarefas.filter(tarefa => tarefa.nome.includes(nome))
+    atualizar()
+})
+
+document.querySelector("#concluidas").addEventListener("click", () => {
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefas")) || []
+    lista_tarefas = lista_tarefas.filter(tarefa => tarefa.concluida)
     atualizar()
 })
 
@@ -22,54 +42,82 @@ function filtrar(lista) {
 }
 
 function cadastrar() {
-    const Nome = document.querySelector("#nome").value;
-    const Marca = document.querySelector("#marca").value;
-    const Escala = document.querySelector("#categoria").value;
-    const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
+    let nome = document.querySelector("#nome").value;
+    let marca = document.querySelector("#marca").value;
+    let categoria = document.querySelector("#categoria").value;
+    const modal = bootstrap.Modal.getInstance(
+        document.querySelector("#exampleModal"))
 
     const tarefa = {
         id: Date.now(),
-        Nome,
-        Marca,
-        Escala,
+        nome,
+        marca,
+        categoria,
         concluida: false
     }
 
-    if (!isValid(tarefa.Nome, document.querySelector("#nome"))) return
-    if (!isValid(tarefa.Marca, document.querySelector("#marca"))) return
+    if (tarefa.nome.length == 0){
+        document.querySelector("#nome").classList.add("is-invalid")
+        return
+    }
 
-    tarefas.push(tarefa)
+    lista_tarefas.push(tarefa)
 
-    atualizar()
+    document.querySelector("#tarefas").innerHTML += criarCard(tarefa)
+    document.querySelector("nome").value = ""
+    document.querySelector("marca").value = ""
+
+    
+    salvar()
     modal.hide()
+}
+
+    // if (!isValid(tarefa.Nome, document.querySelector("#nome"))) return
+    // if (!isValid(tarefa.Marca, document.querySelector("#marca"))) return
+
+    // tarefas.push(tarefa)
+
+    // atualizar()
+    // modal.hide()
 
     //   document.querySelector("#tarefas").innerHTML += criarCard(tarefa);
+
+
+// function isValid(valor, campo) {
+//     if (valor.length == 0) {
+//         campo.classList.add("is-invalid")
+//         campo.classList.remove("is-valid")
+//         return false
+//     } else {
+//         campo.classList.add("is-valid")
+//         campo.classList.remove("is-invalid")
+//         return true
+//     }
+// }
+
+function atualizar(){
+    document.querySelector("#tarefas").innerHTML = ""
+    lista_tarefas.forEach((tarefa) => {
+        document.querySelector("tarefas");innerHTML += criarCard(tarefa)
+    })
 }
 
-function isValid(valor, campo) {
-    if (valor.length == 0) {
-        campo.classList.add("is-invalid")
-        campo.classList.remove("is-valid")
-        return false
-    } else {
-        campo.classList.add("is-valid")
-        campo.classList.remove("is-invalid")
-        return true
-    }
-
-    function apagar(id) {
-        tarefas = tarefas.filter(tarefa => tarefa.id !== id)
-        atualizar()
-    }
-
-    function concluir(id) {
-        let tarefaEncontrada =
-            tarefas.find(tarefa => tarefa.id == id)
-        tarefaEncontrada.concluida = true
-        atualizar()
-    }
+function salvar(){
+    localStorage.setItem("lista_tarefas", JSON.stringify(lista_tarefas))
 }
 
+function apagar(id) {
+    lista_tarefas = lista_tarefas.filter(tarefa => tarefa.id !== id)
+    salvar()
+    atualizar()
+}
+
+function concluir(id) {
+    let tarefa_encontrada = lista_tarefas.find(tarefa => tarefa.id == id)
+    tarefa_encontrada.concluida = true
+    salvar()
+    atualizar()
+}
 // function apagar(botao) {
 //   botao.parentNode.parentNode.parentNode.remove();
 // }
@@ -81,12 +129,12 @@ function criarCard(tarefa) {
             <div class="col-lg-3 col-md-6 col-sm-12">
             <div class="card">
                 <div class="card-header">
-                    ${tarefa.Nome}
+                    ${tarefa.nome}
                 </div>
                 <div class="card-body">
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text">${tarefa.Escala}</p>
-                    <span class="badge text-bg-warning">${tarefa.Marca}pt</span>
+                    <p class="card-text">${tarefa.categoria}</p>
+                    <span class="badge text-bg-warning">${tarefa.marca}pt</span>
                 </div>
                 <div class="card-footer">
                     <a onClick="concluir(${tarefa.id})" href="#" class="btn btn-success ${disabled}" title="marcar como concluída">
